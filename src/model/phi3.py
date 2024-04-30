02449350ab47be344bf31e5db0f292196228cf08
ostix360 <vicrobin1001@gmail.com>
2024-04-30 19:55:48 +0200

    remove unnecessary code and add batch size to avoid OOM

--- src/model/phi3.py
+++ src/model/phi3.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import torch
+import numpy as np
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from ..config import INSTRUCT_PHI3
@@ -15,12 +16,9 @@ class Phi3Model:
         self.tokenizer = AutoTokenizer.from_pretrained(
             "microsoft/Phi-3-mini-128k-instruct", trust_remote_code=True)
         self.model = AutoModelForCausalLM.from_pretrained(
-            "microsoft/Phi-3-mini-128k-instruct", torch_dtype=torch.float16, device_map="auto", trust_remote_code=True)
-        self.tokenizer.padding_side = "left"
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.model.config.pad_token_id = self.model.config.eos_token_id
+            "microsoft/Phi-3-mini-128k-instruct", torch_dtype=torch.bfloat16, device_map="auto", trust_remote_code=True)
 
-    def generate_response(self, prompts: List[Prompt] | Prompt) -> List[Prompt]:
+    def generate_response(self, prompts: List[Prompt] | Prompt, ) -> List[Prompt]:
         """Method to generate response from the model.
 
         :param prompts: the prompts that will be used to generate the response
@@ -30,6 +28,7 @@ class Phi3Model:
         """
 
         msgs = []
+        batch_size = 31     # for 32 batch sizes
 
         # convert the prompt to the format that the model can understand
         if isinstance(prompts, list):
@@ -39,12 +38,17 @@ class Phi3Model:
                         [INSTRUCT_PHI3, {"role": "user", "content": prompt.content}], add_generation_prompt=True, return_tensors="pt", tokenize=False)
                 )
         else:
-            msgs = self.tokenizer.apply_chat_template(
+            msgs.append(self.tokenizer.apply_chat_template(
                 [INSTRUCT_PHI3, {"role": "user", "content": prompts.content}], add_generation_prompt=True, return_tensors="pt", tokenize=False)
-
-        inputs = self.tokenizer(
-            msgs, return_tensors="pt", padding=True).to(self.model.device)
-        output_sequences = self.model.generate(**inputs, max_new_tokens=132)
+            )
+        output_sequences = torch.Tensor([]).to(self.model.device)
+        msgs_batch = np.array_split(np.array(msgs), len(msgs) // batch_size + 1)
+        for msgs in msgs_batch:
+            if msgs.size == 0:
+                continue
+            inputs = self.tokenizer(
+                msgs, return_tensors="pt", padding=True).to(self.model.device)
+            output_sequences = torch.cat(output_sequences, self.model.generate(**inputs, max_new_tokens=512))
 
         # decode the output and remove the input prompt
         output = []
