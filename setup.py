0ee123b82892d09ecec4709ad8a91d5d78d83f71
Jourdelune <jourdelune863@gmail.com>
2024-05-12 13:30:22 +0200

    [fix] add default python version

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import functools
 import os
 import platform
+import re
 import subprocess
+import sys
 from enum import Enum
 from typing import List, Tuple
 from urllib.parse import quote, urlparse
@@ -84,12 +86,48 @@ def get_cpu_avx_support() -> Tuple[OSType, AVXType]:
     return os_type, env_cpu_avx if env_cpu_avx else cpu_avx
 
 
-def get_cuda_version() -> str:
+
+def get_cuda_version_from_torch():
     try:
         import torch
 
         return torch.version.cuda
     except:
+        return None
+
+
+def get_cuda_version_from_nvcc():
+    try:
+        output = subprocess.check_output(["nvcc", "--version"])
+        version_line = [
+            line for line in output.decode("utf-8").split("\n") if "release" in line
+        ][0]
+        return version_line.split("release")[-1].strip().split(",")[0]
+    except:
+        return None
+
+
+def get_cuda_version_from_nvidia_smi():
+    try:
+        output = subprocess.check_output(["nvidia-smi"]).decode("utf-8")
+        match = re.search(r"CUDA Version:\s+(\d+\.\d+)", output)
+        if match:
+            return match.group(1)
+        else:
+            return None
+    except:
+        return None
+
+
+def get_cuda_version() -> str:
+    try:
+        cuda_version = get_cuda_version_from_torch()
+        if not cuda_version:
+            cuda_version = get_cuda_version_from_nvcc()
+        if not cuda_version:
+            cuda_version = get_cuda_version_from_nvidia_smi()
+        return cuda_version
+    except Exception:
         return None
 
 
@@ -105,19 +143,12 @@ setup_spec = SetupSpec()
 
 def llama_cpp_python_cuda_requires():
     cuda_version = get_cuda_version()
-    supported_cuda_versions = ["11.8", "12.1"]
+ 
     device = "cpu"
     if not cuda_version:
         print("CUDA not support, use cpu version")
         return
 
-    # Supports GPU acceleration
-    if cuda_version <= "11.8" and not None:
-        device = "cu" + supported_cuda_versions[0].replace(".", "")
-    else:
-        device = "cu" + supported_cuda_versions[-1].replace(".", "")
-        
-    print(device)
     os_type, cpu_avx = get_cpu_avx_support()
     print(f"OS: {os_type}, cpu avx: {cpu_avx}")
     supported_os = [OSType.WINDOWS, OSType.LINUX]
@@ -129,7 +160,7 @@ def llama_cpp_python_cuda_requires():
 
     base_url = "https://github.com/abetlen/llama-cpp-python/releases/download"
     llama_cpp_version = "0.2.73"
-    py_version = "cp310"
+    py_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
     os_pkg_name = "linux_x86_64" if os_type == OSType.LINUX else "win_amd64"
     extra_index_url = f"{base_url}/v{llama_cpp_version}-cu124/llama_cpp_python-{llama_cpp_version}-{py_version}-{py_version}-{os_pkg_name}.whl"
    
